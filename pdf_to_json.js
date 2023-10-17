0b043ba42e897d8df3cc3b826813a6247b8cb715
Parisa Sadeghi <pariisadeqii@gmail.com>
2023-10-17 16:40:16 +0200

    adding font console.log

--- pdf_to_json.js
+++ pdf_to_json.js
@@ -54,30 +54,30 @@ function process_x(textContent) {
 }
 
 
-function process_styles(textContent) {
-	// parser that loops through all the text objects in the pdf
-	// let lastY, text = ' ';
-	// for (let item of textContent.items) {
-	// 	item_x = item.transform[4]
-	// 	item_y = item.transform[5]
-	// 	item_str = item.str
-	// 	console.log(item_str)
-	// }
-	// return text;
-	let font, text = '';
-	for (let item of textContent.styles) {
-		if (font == item.styles || !font){
-			text += item.str;
-		}
-		else{
-			text += '\n' + item.str;
-		}
-		font = item.styles;
-	}
-	console.log(text)
-	//fs.writeFileSync('output.txt', JSON.stringify(text));
-	return text
-}
+// function process_styles(textContent) {
+// 	// parser that loops through all the text objects in the pdf
+// 	// let lastY, text = ' ';
+// 	// for (let item of textContent.items) {
+// 	// 	item_x = item.transform[4]
+// 	// 	item_y = item.transform[5]
+// 	// 	item_str = item.str
+// 	// 	console.log(item_str)
+// 	// }
+// 	// return text;
+// 	let font, text = '';
+// 	for (let item of textContent.styles) {
+// 		if (font == style.ascent || !font){
+// 			text += style.str;
+// 		}
+// 		else{
+// 			text += '\n' + style.str;
+// 		}
+// 		font = textContent.styles[style.ascent;
+// 	}
+// 	console.log(text)
+// 	//fs.writeFileSync('output.txt', JSON.stringify(text));
+// 	return text
+// }
 
 function process_text_content(textContent) {
 	// parser that loops through all the text objects in the pdf
@@ -93,6 +93,7 @@ function process_text_content(textContent) {
 	for (let item of textContent.items) {
 		if (lastY == item.transform[5] || !lastY){
 			text += item.str;
+			console.log(textContent.styles[item.fontName])
 		}
 		else{
 			text += '\n' + item.str;
