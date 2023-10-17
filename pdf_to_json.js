59f1dad6c1e076af5589724ae6c8ff09084e4b57
Brian Johnson <brian@sherbang.com>
2023-10-17 12:22:45 +0200

    Remove nonprinting characters that are causing errors

--- pdf_to_json.js
+++ pdf_to_json.js
@@ -16,23 +16,21 @@ const PDFParser = require('pdf-parse');
 // parsePDF().then((sections) => {
 // 	console.log(sections);
 // });
-​
-​
- const parsePDF = async () => {
+
+const parsePDF = async () => {
  	const pdfBuffer = fs.readFileSync(pdfFilePath);
  	const pdf = await PDFParser(pdfBuffer);
-​
  	const text = pdf.text;
-​
+
  	const sections = {
  		education: {},
  		workExperience: {},
  		skills: {}
  	};
  	 //console.log(text);
- 	//console.log(pdf);
-​ 	fs.writeFileSync('output.txt', JSON.stringify(pdf));
-​
+	//console.log(pdf);
+	fs.writeFileSync('output.txt', JSON.stringify(pdf));
+
 // ​ fs.readFile(pdfFilePath, (err, pdfBuffer) => {
 // 	//pdfBuffer contains the file content
 // 	new PdfReader().parseBuffer(pdfBuffer, (err, item) => {
@@ -40,7 +38,6 @@ const PDFParser = require('pdf-parse');
 //  		else if (!item) console.warn("end of buffer");
 //  		else if (item.text) console.log(item.text);
 //  	});
-//  });
-​
+//  });​
  	return sections;
 };
